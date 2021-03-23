697769d949e32c41e628682549183c38c0ab9ad7
Antonio Fernandes <vaf888@yahoo.com>
2021-03-23 19:50:41 -0400

    first project in VS2019

---
+++ csharp/CsharpReview/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
