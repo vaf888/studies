f635a79f0c3d1a83b9647a803cc7d712deddc3a5
Antonio Fernandes <vaf888@yahoo.com>
2021-04-08 12:53:22 -0400

    more delegates

---
+++ csharp/Delegate/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Diagnostics;
+
+
+/*
+    --- DELEGATES ---
+    (*) VIP: delegates - not used anymore; it is OLD STYLE - replaced by LAMBDAS !
+  
+    a) delegates are TYPES (as classes, interfaces, etc) 
+       ..they describe a common set of functions (a set of functions that has the
+         same prototype)
+    b) Any lambda expression can be converted to a delegate type
+       ..The delegate type to which a lambda expression can be converted is defined by the types of its parameters and return value.
+       ..If a lambda expression doesn't return a value, it can be converted to one of the Action delegate types
+       ..otherwise, it can be converted to one of the Func delegate types. 
+       ..For example:
+         -a lambda expression that has two parameters and RETURNS NO VALU can be converted 
+          to an Action<T1,T2> delegate.
+         -A lambda expression that has one parameter and RETURNS A VALUE can be converted to a 
+          Func<T,TResult> delegate.
+         -In the following example, the lambda expression x => x * x, which specifies a parameter 
+          that's named x and returns the value of x squared, is assigned to a variable of a 
+          delegate type:
+    c) delegate int MathOp(int x, int y); 
+       ..this delegate is a TYPE - I can declare variables of this type, for instance
+       ..this is a type definition for a category of functions
+
+    d) CalculateAndPrint(2, 3, delegate(int a, int b){return a + b;});
+       ..this delegate is different from (c) - it allow-us to write
+         an anonymous function (lambda) - name is chosen by the compiler
+
+    d1) transforming (d1) lambda:
+        CalculateAndPrint(2, 3, (int a, int b) => {return a + b;});
+        (*) it is very similar tho (d) - the only differences:
+            ..does not use delegate keyword
+            ..use =>   (fat arrow)
+            ..HAS NO NAME (is anonymous) - the compiler will define the name !
+
+        (**) simplification-1 -> if I have a single line of code in the lambda
+             from: (int a, int b) => {return a + b;}
+             to: (int a, int b) => a + b
+             (*) if we have multiple lines in the code, we still need all {...}
+
+        (**) simplification-2 -> CalculateAndPrint(2, 3, a,b => a + b);
+             ..no need to add the types, as the compiler can deduce them from
+               the prototype of the CalculateAndPrint function:
+               -3rd parameter is a delegate that has 2 ints as input !!!
+        (**) Now, we have a lambda function - it is just a delegate (simplified).
+
+    e) Ildasm.exe and Ilasm.exe
+       ..Ildasm    -> The IL(intermediate language) Disassembler
+        -takes a portable executable (PE) file that contains intermediate language (IL) code and 
+         creates a text file suitable as input to Ilasm.exe
+        -use ILDASM to disassemble assembly (either exe or dll)
+        -to view the code/content of the intermediate language,  in C#.NET format
+        -will generate a text file with MANIFEST
+        --will allow to generate the assembly metadata and IL code (export to a text file)
+
+       ..Ilasm.exe -> IL Assembler
+        -use ILASM to reassemble assembly
+        -to rebuild an assembly from IL
+        -to rebuild an assembly from a text file which contains manifest and IL
+        
+    f) What is Manifest for assembly (*.dll or *.exe)?
+       ..Manifest contains metadata about the assembly like the name of the assembly, 
+         the version number of the assembly, culture, OS, and strong name information, etc.
+
+    g) IL - intermediate language
+       ..machine and platform independent 
+       ..just before running, IL is transformed to executable (machine language) according 
+         to the platform
+       ..JIT compiler transforms C# code in IL code 
+
+    h) generic delegate:
+       delegate T CombineGeneric<T>(T a, T b);
+
+*/
+class Program
+{
+    //-------------------------------------------------
+    delegate int MathOp(int x, int y); //(1) - this delegate is a type - I can have vars. of it
+
+    static int add(int x1, int y1)
+    {
+        return x1 + y1;
+    }
+
+    static int subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    //passing a delegate as a parameter
+    static void CalculateAndPrint(int x, int y, MathOp f )
+    {
+        var res = f(x, y);
+        Console.WriteLine("CalculateAndPrint({0},{1})={2}; f={3}", x,y,res,f.Method.ToString());
+    }
+
+    //--------------------------------------------------
+    // Delegate + Generics !!!
+    delegate T CombineGeneric<T>(T a, T b);
+
+    static double add(double x1, double y1)
+    {
+        return x1 + y1;
+    }
+
+    static void CalculateAndPrintGen<T>(T x, T y, CombineGeneric<T> f)
+    {
+        var res = f(x, y);
+        Console.WriteLine("CalculateAndPrintGen({0},{1})={2}; f={3}", x, y, res, f.Method.ToString());
+    }
+
+    //--------------------------------------------------
+    static void Main(string[] args)
+    {
+        //using the delegate
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>calling delegate...");
+        MathOp f = add;
+        var res = f(2, 3);
+        Debug.Assert(res == 5);
+        Console.WriteLine("f(2+3)={0}", res);
+
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>calling delegate...");
+        f = subtract;
+        res = f(100, 90);
+        Debug.Assert(res == 10);
+        Console.WriteLine("f(100-90)={0}", res);
+
+        //using the CalculateAndPrint
+        //using the delegate
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>passing delegate as parameter...");
+        f = add;
+        CalculateAndPrint(2, 3, f);
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>passing delegate as parameter...");
+        f = subtract;
+        CalculateAndPrint(100, 90, f);
+
+        //call using the DELEGATE INLINED !
+        //return type can be deduced !!!
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>using delegate - inline...");
+        CalculateAndPrint(2, 3, delegate(int a, int b){return a + b;});
+
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>using delegate - inline...");
+        CalculateAndPrint(100, 10, delegate (int a, int b)
+            {
+                return a - b;
+            }
+        );
+
+        //using lambdas - to inline method to call - to define the method (delegate parameter)
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>using lambdas...");
+        CalculateAndPrint(2, 3, (int a, int b) => { return a + b; });
+
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>using lambdas...");
+        CalculateAndPrint(100, 90, (int a, int b) => { return a - b; });
+
+        //using delegate + generics
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>calling delegate generic...");
+        CombineGeneric<int> gn = add;
+        res = gn(2, 3);
+        Debug.Assert(res == 5);
+        Console.WriteLine("gn(2+3)={0}", res);
+
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>calling delegate generic (double)...");
+        CombineGeneric<double> gnd = add;
+        double resd = gnd(2.5, 3.5);
+        Console.WriteLine("gn(2.5+3.5)={0}", resd);
+
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>using CalculateAndPrintGen<int>...");
+        CalculateAndPrintGen<int>(100, 90, (a, b) => { return a + b; });
+
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>using CalculateAndPrintGen<double>...");
+        CalculateAndPrintGen<double>(10.0, 20.0, (a, b) => a + b);
+
+        Console.WriteLine("__________________________________________");
+        Console.WriteLine(">>>>>using CalculateAndPrintGen - boolean...");
+        CalculateAndPrintGen(true, false, (a, b) => a && b);
+
+    }
+
+}
+
+
+/*
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+*/
---
+++ csharp/DelegateAdvancedNet5/DelegateAdvancedNet5/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+
+//--- .net5.0 & c# 9.0 ---
+//    ..no need of main, classes , for short demos - called top level statements
+
+// a) using IEnumerable
+//    VIP: we can use IEnumerable because we will not change the content of the list !!!
+//
+// b) Pre-defined delegates - via Func
+//    -Func is built-in delegate type. 
+//
+// Func<T,TReturn>  -> e.g.: Func<T,bool>
+//  T = input parameter
+//  TReturn = return value
+// 
+// e.g.: from: IEnumerable<T> FilterHeroesFunc<T>(IEnumerable<T> items, Filter<T> f)
+//             (*) where Filter<T> -> delegate bool Filter<T>(T a);
+//                 a delegate that returns a bool and has T as input
+//         The corresponding pre-defined delegate:
+//         to: IEnumerable<T> FilterHeroesFunc<T>(IEnumerable<T> items, Func<T,bool> f)
+//
+// in VS is defined as:
+// -C# includes built-in generic delegate types Func and Action
+//  (so that you don't need to define custom delegates manually in most cases.)
+// b1) Func:
+//    ..Func is a generic delegate included in the System namespace.
+//    ..It has zero or more input parameters and one out parameter.
+//    ..The last parameter is considered as an out parameter.
+//    b11) The Func delegate that takes one input parameter and one out parameter is defined
+//         in the System namespace, as shown below:
+//          namespace System
+//          {
+//              public delegate TResult Func<in T, out TResult>(T arg);
+//          }
+//    b12) A Func delegate takes two input parameters of int type and returns a value
+//         of int type:
+//         Func<int, int, int> sum;
+//         (*) A Func delegate type can include 0 to 16 input parameters of different type
+//             and MUST include the out parameter.
+//             e.g.: Func with Zero Input Parameter
+//                   Func<int> getRandomNumber;
+//
+//
+// c) Func + lambda expressions:
+//        Func<int> getRandomNumber = () => new Random().Next(1, 100);
+//        and
+//        Func<int, int, int> Sum = (x, y) => x + y;
+//
+// d) Delegate type Action
+//    Action = pre-defined delegate that receives NOTHING and returns NOTHING
+//      E.g.:
+//      delegate void Something(); -> the pre-defined value is:  Action Something
+// 
+
+var heroes = new List<Hero>
+{
+    new ( "F1", "L1", "Batman", true),
+    new ( "F3", "L3", "Mandrake", false),
+    new ( "F2", "L2", "Robin", true),
+    new ( "F4", "L4", "Robin Hood", false),
+    new ( "F5", "L5", "Superman", true),
+    new ( string.Empty, string.Empty, "TESTS ONLY", false),
+    new ( string.Empty, string.Empty, "TESTS ONLY 2", true)
+};
+
+List<Hero> FilterHeroesCanFly1(List<Hero> heroes )
+{
+    List<Hero> res = new List<Hero>();
+
+    foreach(var hero in heroes)
+    {
+        if (hero.CanFly)
+            res.Add(hero);
+    }
+
+    return res;
+}
+
+//using IEnumerable
+//VIP: we can use IEnumerable because we will not change the content of the list !!!
+IEnumerable<Hero> FilterHeroesCanFly2(IEnumerable<Hero> heroes)
+{
+    foreach (var hero in heroes)
+    {
+        if (hero.CanFly)
+            yield return hero;
+    }
+   
+}
+
+IEnumerable<Hero> FilterHeroesFnIsEmpty(IEnumerable<Hero> heroes)
+{
+    foreach (var hero in heroes)
+    {
+        if (String.IsNullOrEmpty(hero.FirsName))
+            yield return hero;
+    }
+}
+
+
+//using all generic - method + delegate    
+IEnumerable<T> FilterHeroes<T>(IEnumerable<T> items, Filter<T> f) 
+{
+    foreach (var item in items)
+    {
+        if(f(item))
+            yield return item;
+    }
+}
+
+//using pre-defined delegate Func (returns a bool & has T as input)
+IEnumerable<T> FilterHeroesFunc<T>(IEnumerable<T> items, Func<T,bool> f)
+{
+    foreach (var item in items)
+    {
+        if (f(item))
+            yield return item;
+    }
+}
+
+//action delegate
+void PrintMessage()
+{
+    Console.WriteLine(">>>>>Hello from PrintMessage");
+}
+
+bool RunPrint(Action a)
+{
+    a();
+    return true;
+}
+
+
+Console.WriteLine("__________________________________________");
+Console.WriteLine(">>>>>using FilterHeroesCanFly1...");
+var res = FilterHeroesCanFly1(heroes);
+foreach( var hero in res)
+{
+    Console.WriteLine($"Fn={hero.FirsName}; Ln={hero.LastName}; Name={hero.HeroName}; CanFly={hero.CanFly}");
+}
+
+Console.WriteLine("__________________________________________");
+Console.WriteLine(">>>>>using FilterHeroesCanFly2...");
+var res2 = FilterHeroesCanFly2(heroes);
+foreach (var hero in res2)
+{
+    Console.WriteLine($"Fn={hero.FirsName}; Ln={hero.LastName}; Name={hero.HeroName}; CanFly={hero.CanFly}");
+}
+
+Console.WriteLine("__________________________________________");
+Console.WriteLine(">>>>>using FilterHeroesFnIsEmpty...");
+var res3 = FilterHeroesFnIsEmpty(heroes);
+foreach (var hero in res3)
+{
+    Console.WriteLine($"Fn={hero.FirsName}; Ln={hero.LastName}; Name={hero.HeroName}; CanFly={hero.CanFly}");
+}
+
+//using all generic - method + delegate    
+Console.WriteLine("__________________________________________");
+Console.WriteLine(">>>>>using Generics: FilterHeroes + FnIsEmpty ...");
+var res4 = FilterHeroes<Hero>(heroes, h => String.IsNullOrEmpty(h.FirsName));
+foreach (var hero in res4)
+{
+    Console.WriteLine($"Fn={hero.FirsName}; Ln={hero.LastName}; Name={hero.HeroName}; CanFly={hero.CanFly}");
+}
+
+Console.WriteLine("__________________________________________");
+Console.WriteLine(">>>>>using Generics: FilterHeroes + CanFly ...");
+var res5 = FilterHeroes<Hero>(heroes, h => h.CanFly);
+foreach (var hero in res5)
+{
+    Console.WriteLine($"Fn={hero.FirsName}; Ln={hero.LastName}; Name={hero.HeroName}; CanFly={hero.CanFly}");
+}
+
+//using pre-defined delegate
+Console.WriteLine("__________________________________________");
+Console.WriteLine(">>>>>using pre-defined delegate + CanFly ...");
+var res6 = FilterHeroesFunc<Hero>(heroes, h => h.CanFly);
+foreach (var hero in res6)
+{
+    Console.WriteLine($"Fn={hero.FirsName}; Ln={hero.LastName}; Name={hero.HeroName}; CanFly={hero.CanFly}");
+}
+
+//using Action
+Console.WriteLine("__________________________________________");
+Console.WriteLine(">>>>>using Action delegate...");
+var res7 = RunPrint(PrintMessage);
+
+//IEnumerable<T> Filter<T>(IEnumerable<T> items, Filter<T> f)
+
+//--- top level definitions after ---
+
+// new feature - record (similar to classes + with auto features)
+// ..no need to add properties/fields, constructors, etc - all is done automatically
+// ..this is a new type of class in .net , that saves you a lot of work
+record Hero (string FirsName, string LastName, string HeroName, bool CanFly);
+//I could add after the (...) a {other constructors, etc - like a regular class} 
+
+// delegate generic
+delegate bool Filter<T>(T a);
