1e42cbcd92f49dbb5ff6f7a1cb9f932cf38ef837
Antonio Fernandes <vaf888@yahoo.com>
2021-04-10 19:59:12 -0400

    linq continuation

---
+++ csharp/Linq/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+    --- LINQ ---
+    
+a) IEnumerable interface
+   ..this is: a readonly, forward only cursor through a collection
+   ..current  -> get the corrent element
+   ..movenext -> go to the next element (returns NULL if no more available!)
+   (*) can't go back, can't access via an index, etc
+   ..Is a pull base system -> The pull-based system waits for 
+     a demand to push the data streams to the requestor
+     (but Observar Pattern - is the contrary - is a push based system
+      subject (observable) pushes its values to observers0
+
+    -No memory used for huge data/list - as item are processed
+     one by one!
+b) VIP: The call to a method that returns an IEnumerable
+        IS NOT immediately executed - is delayed - only
+        when I try to access the content of the result
+        (e.g.: inside a foreach) - the method will be called
+        using yield - to process 1 item at time
+
+   
+*/
+
+//--------------------------------------------------
+IEnumerable<int> GenerateNumbers1(int maxValue)
+{
+    var result = new List<int>();
+
+    for(var i=0; i <= maxValue; i++)
+    {
+        result.Add(i);
+    }
+
+    //I can return a list because LIST implements IEnumerable
+    return result;
+}
+
+//--------------------------------------------------
+//removing list
+IEnumerable<int> GenerateNumbers2(int maxValue)
+{
+    //This method does not generate the entire sequence, only 
+    //one at time
+    for (var i = 0; i <= maxValue; i++)
+    {
+        // yield - returns the result to caller without
+        // having to create a LIST
+        // returns 1 item at time
+        yield return i*5;
+    }
+}
+
+//--------------------------------------------------
+//linq-1 -> return even numbers only
+IEnumerable<int> LinqEvenNumbersOnly()
+{
+    var result = GenerateNumbers2(5).Where(n => n % 2 == 0);
+    return result;
+}
+
+//--------------------------------------------------
+//DEBUG: How IEnumerable does its processing with LINQ
+void LinqHowIEnumerableLinqWorks()
+{
+    //VIP: GenerateNumbers2(5) was NOT CALLED here
+    //     it will be called ONLY in the line below
+    //     foreach (var item in result)
+    //     and 1 time for each item (10 times)
+    //     The call to GenerateNumbers2 occurs INSIDE
+    //     the foreach loop ONLY ! (is delayed)
+
+    //each new item received from GenerateNumbers2 - via yield
+    //runs the intire pipeline(*) (before getting a new value):
+    //(*) for each item got via yield:
+    //(1) ..runs the where
+    //..runs the select
+    //..return value after call in foreach
+    //..write item
+    //..RETURNS BACK AFTER the yield (inside for loop) - to get next value
+    //..and process it (1)
+
+    // ---normal version---
+    //var result = GenerateNumbers2(5)
+    //    .Where(n => n % 2 == 0)
+    //    .Select(n =>  n * 3); // PROJECTION
+
+    // ---for debug only - expanded lambdas ---
+    var result = GenerateNumbers2(5)
+        .Where(n =>
+            {
+                return n % 2 == 0;
+            }
+        )
+        .Select(n => // PROJECTION
+        {
+            return n * 3;
+        }
+        );
+
+    // only at this point, when we access result
+    // is that GenerateNumbers2(5) will be called
+    //VIP: this will call GenerateNumbers2(5) 10 times
+    //     it means it will calculate ONE item at a time
+    //     (the yield will run as part of each MoveNext in
+    //      IEnumerable)
+    //
+    // When the foreach starts running the MoveNext method
+    // of the IEnumerable is called for the first time ->
+    // then, the GenerateNumbers2 is called (not before)
+
+    foreach (var item in result)
+    {
+        Console.WriteLine($"{item}, ");
+    }
+}
+
+//--------------------------------------------------
+void LinqIEnumerableBreakingQuery()
+{
+    // Even if I break the query sequence - the result will
+    // be the same - call to GenerateNumbers2 will be done
+    // only in the foreach - when I need the content
+    //e.g.
+
+    // the same pipeline will be executed each time
+    // we get a new value from GenerateNumbers2 + yield
+    // (called in the foreach loop)
+
+    //here I can control which query steps I will run
+    // this is called query composition
+    bool even = false;
+
+    var result = GenerateNumbers2(5);
+    if (even)
+    {
+        result = result.Where(n => n % 2 == 0);
+    }
+    result = result.Select(n => n * 3);
+
+    // at this point - will use IEnumerable result (we need it),
+    // will do calls to GenerateNumbers2(5) + yield to process
+    // each value, one by one (so, it never load all results in memory
+    // to process)
+    foreach (var item in result)
+    {
+        Console.WriteLine($"{item}, ");
+    }
+}
+
+//--------------------------------------------------
+void LinqImmediateCalls()
+{
+    // Some methods will call immediately the function
+    // (no delay); e.g.:
+
+    //count
+
+    bool even = true;
+
+    // It seems it runs 3 x the query ??? check this:
+    // 1-when start function + queries (pipeline) ???????????
+    // 2-when count is called
+    // 3-when foreach is called
+
+    //because we use result.Count below - this time full
+    //query was processed (updating content of result)
+    var result = GenerateNumbers2(5);
+    //e.g.: result=0,5,10,15,20,25
+    if (even)
+    {
+        result = result.Where(n => n % 2 == 0);
+    }
+    result = result.OrderByDescending(n => n);
+    result = result.Select(n => n * 7);
+    //at this point: result content = 0,30,60 and count = 3
+
+    //at this point - result is already correct (with all values)
+    //so that count can use it
+    Console.WriteLine($"number of items={result.Count()}");
+
+    //??? called function + pipeline again ???
+    foreach (var item in result)
+    {
+        Console.WriteLine($"{item}, ");
+    }
+}
+
+//--------------------------------------------------
+void LinqImmediateCallsTest()
+{
+    // Some methods will call immediately the function
+    // (no delay); e.g.:
+
+    //count
+    
+    var result = GenerateNumbers2(5)
+        .Where(n => n % 2 == 0)
+        .OrderByDescending(n => n)
+        .Select(n => n * 3);
+
+    //so that count can use it
+    Console.WriteLine($"number of items={result.Count()}");
+
+    foreach (var item in result)
+    {
+        Console.WriteLine($"{item}, ");
+    }
+}
+
+
+//--------------------------------------------------
+//--------------------------------------------------
+Console.WriteLine("_________________________________");
+Console.WriteLine($">>>>>GenerateNumbers1(10)...");
+foreach (var item in GenerateNumbers1(10))
+{
+    Console.WriteLine($"{item}, ");
+}
+
+//Console.WriteLine("_________________________________");
+//Console.WriteLine($">>>>>GenerateNumbers2(5)...");
+//foreach (var item in GenerateNumbers2(5))
+//{
+//    Console.WriteLine($"{item}, ");
+//}
+
+//Console.WriteLine("_________________________________");
+//Console.WriteLine($">>>>>LinqEvenNumbersOnly()...");
+//foreach (var item in LinqEvenNumbersOnly())
+//{
+//    Console.WriteLine($"{item}, ");
+//}
+
+Console.WriteLine("_________________________________");
+Console.WriteLine($">>>>>LinqHowIEnumerableLinqWorks()...");
+LinqHowIEnumerableLinqWorks();
+
+Console.WriteLine("_________________________________");
+Console.WriteLine($">>>>>LinqIEnumerableBreakingQuery()...");
+LinqIEnumerableBreakingQuery();
+
+Console.WriteLine("_________________________________");
+Console.WriteLine($">>>>>LinqImmediateCalls()...");
+LinqImmediateCalls();
+
+Console.WriteLine("_________________________________");
+Console.WriteLine($">>>>>LinqImmediateCallsTest()...");
+LinqImmediateCallsTest();
+
---
+++ csharp/LinqOperators/Program.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+
+//.net5.0 -> no need main + namespace for tests
+
+// Json serialization:
+// - Newtonsoft.Json -> not used anymore
+//   use System.Text.Json instead !!!
+
+// How GroupBy works:
+// eg. car.GroupBy(car => car.Make)
+// 1) first, it sorts data by the column you want to groupe
+//    (car.Make, in the example)
+// 2) it goes to all distinct items, and put them in a group
+//    (it generates Group Keys column - unique - pointing to the Key of
+//     the original data - car.Model in this example
+// 3) remove all duplications in the grop key generated
+//    this way, we will have one Group Key item pointing
+//    to the original data (N items) - 1:N relationship
+//     
+//-------------------------------------------
+async Task<CarData[]> DeserializeJsonFileAsync()
+{
+    const string FilePath = "D:\\gitrepo\\studies\\csharp\\LinqOperators\\data.json";
+
+    var fileContent = await 
+        File.ReadAllTextAsync(FilePath);
+    var cars = JsonSerializer.Deserialize<CarData[]>(fileContent);
+
+    return cars;
+}
+
+//-------------------------------------------
+void DisplayCars(IEnumerable<CarData> cars, int maxNumber)
+{
+    Console.WriteLine($"list of cars available...");
+    Console.WriteLine($"Printing at most -> {maxNumber} of the total found={cars.Count()}");
+
+    int i = 0;
+    foreach (var car in cars)
+    {
+        Console.WriteLine($"id={car.ID}; make={car.Make}; model={car.Model}; year={car.Year}; number_doors={car.NumberOfDoors}; hp={car.HP}");
+        if ((++i) >= maxNumber)
+            break;
+    }
+
+}
+
+void DisplayCarsFromString(IEnumerable<string> cars, string label, int maxNumber)
+{
+    Console.WriteLine($"list of cars available...");
+    Console.WriteLine($"Printing at most -> {maxNumber} of the total found={cars.Count()}");
+
+    //int i = 0;
+    //Console.WriteLine($"   {0}", String.Join(" ", cars));
+
+    var i = 0;
+    foreach (var car in cars)
+    {
+        //var tmp = car.split(" ");
+        Console.WriteLine($"{label} -> {car}");
+        if ((++i) >= maxNumber)
+            break;
+    }
+    //Console.WriteLine($"{String.Join("\n", cars)}");
+}
+
+
+//-------------------------------------------
+void LinqPrintAtLeastCars4Doors(CarData[] cars)
+{
+    var carsWithAtLeast4Doors =
+        cars.Where(cars => cars.NumberOfDoors >= 4);
+
+    DisplayCars(carsWithAtLeast4Doors, 1000);
+}
+
+//-------------------------------------------
+void LinqPrintAllMazdaWithAtLeastCars4Doors(CarData[] cars)
+{
+    var result = cars
+            // slightly more performant !!!
+            //.Where(car => car.NumberOfDoors >= 4 && car.Make.Equals("Mazda"));
+            // OR
+            .Where(car => car.NumberOfDoors >= 4)
+            .Where(car => car.Make.Equals("Mazda"));
+
+    DisplayCars(result, 1000);
+}
+
+//-------------------------------------------
+void LinqPrintAllMakesStartsWithM(CarData[] cars)
+{
+    /*
+    //OPTION-1 -> creating a var. with results, then, print results
+    var result = cars
+            .Where(car => car.Make.StartsWith("M"))
+            // projection - get only part of the CarData fields
+            // were are projecting the CarData to a string
+            // after the select - we don't have an IEnumerable of
+            // CarData anymore - but a IEnumerable of string
+            .Select(car => $"{car.Make} {car.Model}")
+            // ToList -> returns a List<string> - which is
+            // an IEnumerable !!!
+            .ToList();
+    DisplayCarsFromString(result, "make/model", 1000);
+    */
+
+    //OPTION-2 -> query display results automaticaly
+    //            no need of variable (to print later)
+    cars
+    .Where(car => car.Make.StartsWith("M"))
+    // projection - get only part of the CarData fields
+    // were are projecting the CarData to a string
+    // after the select - we don't have an IEnumerable of
+    // CarData anymore - but a IEnumerable of string
+    .Select(car => $"{car.Make} {car.Model}")
+    // ToList -> returns a List<string> - which is
+    // an IEnumerable !!!
+    .ToList()
+    //ForEach available only in List (NOT in IEnumerable)
+    //so, to use ForEach we need the ToList()
+    //ForEach -> does not return anything - is an action
+    .ForEach(car => Console.WriteLine(car));
+}
+
+//-------------------------------------------
+//Display a list of the 10 most powerful cars in
+//terms of HP - show make + model
+void LinqPrint10MostPowerful(CarData[] cars)
+{
+    cars
+    .OrderByDescending(car => car.HP)
+    .Take(10)
+    .Select(car => $"{car.Make}; {car.Model}; {car.HP}")
+    .ToList()
+    .ForEach(car => Console.WriteLine(car));
+}
+
+//---GroupBy---
+
+//-------------------------------------------
+//Display the number of models per make that appears
+//after 1995
+void LinqPrintModelsPerMakeAfter1995(CarData[] cars)
+{
+    cars
+    // the result of a GroupBy is an:
+    // IEnumerable<IGrouping<string, CarData>>
+    // string = auto generated unique Key (make) for grouping
+    // so, per Make, we have a collection of CarData
+    // so, we have in the grouping:
+    // one make -> pointing to a collection of CarData belonging to that make !
+    .Where(car => car.Year > 1995)
+    .GroupBy(car => car.Make)
+    /*
+    //shows all the grouping keys generate by this GroupBy = all unique/distinct car makes
+    .ForEach(grp => Console.WriteLine(grp.Key));
+    */
+    // do a projection - to get the make + number of models (via Count)
+    //.Select(grp => $"{grp.Key} -> {grp.Count<CarData>()}")
+
+    //OR - using anonymous class
+    .Select(grp => new { TheMake = grp.Key, TheNumberOfModels = grp.Count() })
+    .ToList()
+    //.ForEach(item => Console.WriteLine(car));
+    .ForEach(item => Console.WriteLine($"{item.TheMake} -> {item.TheNumberOfModels}"));
+}
+
+//-------------------------------------------
+// Display the number of models per make that appears
+// after 2008 & makes should appear with 0 with
+// there are no models for the selected year
+void LinqPrintModelsPerMakeAfter2008Zeroes(CarData[] cars)
+{
+    cars.GroupBy(car => car.Make)
+    //OR - using anonymous class
+        .Select(grp => new 
+        {   TheMake = grp.Key,
+            // grp.Where - similar to having - filter inside the group
+            //TheNumberOfModels = grp.Where(car => car.Year >= 2008).Count()
+
+            //OR samething but SIMPLER
+            TheNumberOfModels = grp.Count(car => car.Year >= 2008)
+        })
+        .ToList()
+        .ForEach(item => Console.WriteLine($"{item.TheMake} -> {item.TheNumberOfModels}"));
+}
+
+//-----------------------------------------------
+//Display a list of makes that have at least 2 models
+//that appears after 1995
+void LinqPrintAtLeast2ModelsAfter1995(CarData[] cars)
+{
+    cars.Where(car => car.Year > 1995)
+        .GroupBy(car => car.Make)
+        .Select(grp => new
+        {
+            TheMake = grp.Key,
+            //OR samething but SIMPLER
+            TheNumberOfModels = grp.Count()
+        })
+        .Where(grp => grp.TheNumberOfModels >=2)
+        .ToList()
+        .ForEach(item => Console.WriteLine($"{item.TheMake} -> {item.TheNumberOfModels}"));
+}
+
+//-----------------------------------------------
+//Display a list of makes that have at least 2 models
+//with >= 400 HP
+void LinqPrintAtLeast2Models400HP(CarData[] cars)
+{
+    cars.Where(car => car.HP >= 400)
+        .GroupBy(car => car.Make)
+        .Select(grp => new
+        {
+            TheMake = grp.Key,
+            //OR samething but SIMPLER
+            TheNumberOfModels = grp.Count()
+        })
+        //this is similar to a having - in sql
+        .Where(grp => grp.TheNumberOfModels >= 2)
+        .ToList()
+        .ForEach(item => Console.WriteLine($"{item.TheMake} -> {item.TheNumberOfModels}"));
+}
+
+//-----------------------------------------------
+//Display a list of makes that have at least 2 models
+//with >= 400 HP
+void LinqPrintAvgHPPerMake(CarData[] cars)
+{
+    cars.GroupBy(car => car.Make)
+        .Select(grp => new
+        {
+            TheMake = grp.Key,
+            //OR samething but SIMPLER
+            TheAverageHp = grp.Average(grp => grp.HP)
+        })
+        //this is similar to a having - in sql
+        .ToList()
+        .ForEach(item => Console.WriteLine($"{item.TheMake} -> {item.TheAverageHp}"));
+}
+
+//-----------------------------------------------
+//Display how many makes build cars with
+//horse power
+//between 00..100, 101..200, 201..300, 301..400, 401..500
+void LinqPrintHPInRange(CarData[] cars)
+{
+    cars.GroupBy(car => car.HP switch
+        {
+            //building groups based on a calculated
+            //expression/vale
+
+            //the sintaxe below is c#9 - switch expression
+            <= 100 => "000..100",
+            <= 200 => "101..200",
+            <= 300 => "201..300",
+            <= 400 => "301..400",
+            _ =>      "401..500" //else
+        })
+        .Select(grp => new
+        {
+            HPCategory = grp.Key, 
+            NumberOfMaques = grp.Distinct().Count()
+        })
+        .OrderBy(grp => grp.HPCategory)
+        //this is similar to a having - in sql
+        .ToList()
+        .ForEach(item => Console.WriteLine($"{item.HPCategory} -> {item.NumberOfMaques}"));
+}
+
+
+//--- main calls ---
+//-----------------------------------------------
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using DeserializeJsonFileAsync...");
+var allCars = await DeserializeJsonFileAsync();
+Console.WriteLine($"json file content...");
+DisplayCars(allCars, 10);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrintAtLeastCars4Doors()...");
+var allCars2 = await DeserializeJsonFileAsync();
+LinqPrintAtLeastCars4Doors(allCars2);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrintAllMazdaWithAtLeastCars4Doors()...");
+var allCars3 = await DeserializeJsonFileAsync();
+LinqPrintAllMazdaWithAtLeastCars4Doors(allCars3);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrintAllMakesStartsWithM()...");
+var allCars4 = await DeserializeJsonFileAsync();
+LinqPrintAllMakesStartsWithM(allCars4);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrint10MostPowerful()...");
+var allCars5 = await DeserializeJsonFileAsync();
+LinqPrint10MostPowerful(allCars5);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrintModelsPerMakeAfter1959()...");
+var allCars6 = await DeserializeJsonFileAsync();
+LinqPrintModelsPerMakeAfter1995(allCars6);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrintModelsPerMakeAfter2008Zeroes()...");
+var allCars7 = await DeserializeJsonFileAsync();
+LinqPrintModelsPerMakeAfter2008Zeroes(allCars7);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrintAtLeast2ModelsAfter1995()...");
+var allCars8 = await DeserializeJsonFileAsync();
+LinqPrintAtLeast2ModelsAfter1995(allCars8);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrintAtLeast2Models400HP()...");
+var allCars9 = await DeserializeJsonFileAsync();
+LinqPrintAtLeast2Models400HP(allCars9);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrintAvgHPPerMake()...");
+var allCars10 = await DeserializeJsonFileAsync();
+LinqPrintAvgHPPerMake(allCars10);
+
+Console.WriteLine("___________________________");
+Console.WriteLine(">>>>>using LinqPrintHPInRange()...");
+var allCars11 = await DeserializeJsonFileAsync();
+LinqPrintHPInRange(allCars11);
+
+
+//-------------------------------------------
+class CarData
+{
+    //the name of the property in json file is different - so, we fix vai [JsonProperty...]
+    [JsonPropertyName("id")]
+    public int ID { get; set; }
+
+    [JsonPropertyName("car_make")]
+    public string Make { get; set; }
+
+    [JsonPropertyName("car_models")]
+    public string Model { get; set; }
+
+    [JsonPropertyName("car_year")]
+    public int Year { get; set; }
+
+    [JsonPropertyName("number_of_doors")]
+    public int NumberOfDoors { get; set; }
+
+    [JsonPropertyName("hp")]
+    public int HP { get; set; }
+}
+
